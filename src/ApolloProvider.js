d5dc2891501e1efe5ab1c912b249f3667ff9b304
Ujjwal <ujjwal26092002@gmail.com>
2022-05-09 00:51:37 +0530

    add url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://cryptic-brook-06322.herokuapp.com/'
 });
 
 
